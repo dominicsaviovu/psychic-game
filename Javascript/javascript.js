1645bf09a05c959de64b7f583140672eafc6ae98
Savio <savio@Savios-MacBook-Pro.local>
2019-09-09 15:02:40 -0600

    Adding console.log()

--- Javascript/javascript.js
+++ Javascript/javascript.js
@@ -1,3 +1,6 @@
+console.log('Connected');
+
+
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var guessedLetters = [];
 var letterToGuess = null;
