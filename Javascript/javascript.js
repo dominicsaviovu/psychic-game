154dad54fff530e3fa7224f72b817b6dd7bf9e29
Savio <savio@Savios-MacBook-Pro.local>
2019-09-09 13:03:46 -0600

    added the rest of the alphabet

--- Javascript/javascript.js
+++ Javascript/javascript.js
@@ -1,34 +1,34 @@
-var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i",];
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var guessedLetters = [];
 var letterToGuess = null;
 var guessesLeft = 9;
 var wins = 0;
 var losses = 0;
 
-var updateGuessesLeft = function() {
+var updateGuessesLeft = function () {
   document.querySelector("#guesses-left").innerHTML = guessesLeft;
 };
 
-var updateLetterToGuess = function() {
+var updateLetterToGuess = function () {
   letterToGuess = letters[Math.floor(Math.random() * letters.length)];
 };
 
-var updateGuessesSoFar = function() {
+var updateGuessesSoFar = function () {
   document.querySelector("#guesses-so-far").innerHTML = guessedLetters.join(", ");
 };
 
-var reset = function() {
+var reset = function () {
   guessesLeft = 9;
   guessedLetters = [];
   updateLetterToGuess();
   updateGuessesLeft();
   updateGuessesSoFar();
-    };
+};
 
 updateLetterToGuess();
 updateGuessesLeft();
 
-document.onkeydown = function(event) {
+document.onkeydown = function (event) {
   guessesLeft--;
   var letter = event.key.toLowerCase();
   guessedLetters.push(letter);
